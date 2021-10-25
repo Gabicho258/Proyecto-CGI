088720cddb8eca4fff50e2a4347a148c9fbc5804
Gabicho258 <85516522+Gabicho258@users.noreply.github.com>
2021-10-25 12:02:42 -0500

    Proyecto terminado

---
+++ cgi-bin/advancedSearch.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+
+my $q = CGI->new;
+#Proceso para cambiar los espacios por + como en la url de Google
+my $differentWords = $q->param('differentWords');
+$differentWords = join("+", split(" ",$differentWords));
+
+my $wordsExactly = $q->param('wordsExactly');
+$wordsExactly = join("+", split(" ", $wordsExactly));
+
+my $anyWord = $q->param('anyWord');
+$anyWord = join("+", split(" ", $anyWord));
+
+print $q->redirect("https://www.google.com/search?as_q=$differentWords&as_epq=$wordsExactly&as_oq=&as_eq=$anyWord");
+
+
+
+
+
+
+
+
+
+
+
+
+
